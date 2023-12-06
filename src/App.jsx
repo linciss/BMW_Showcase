8f4e0cc75aa845e70b3f462d390553e8a234ff86
lincis <linardssils1@gmail.com>
2023-12-06 14:03:21 +0200

    Simple 3d scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Canvas } from '@react-three/fiber';
 import React, { Suspense } from 'react';
-
+import './style.css';
+import Showcase from './Components/Showcase';
 const App = () => {
   return (
     <Suspense fallback={null}>
       <Canvas shadows>
-        <div>App</div>
+        <Showcase />
       </Canvas>
     </Suspense>
   );
---
+++ src/Components/Showcase.jsx
@@ -0,0 +1,33 @@
+import { Car } from './Car';
+import Ground from './Ground';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+const Showcase = () => {
+  return (
+    <>
+      <OrbitControls target0={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      <color attach="background" args={['#000000']} />
+      <ambientLight intensity={1} />
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={200}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={350}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <Ground />
+    </>
+  );
+};
+export default Showcase;
