04d4b64c75846a471a2ee426776176057de2c5f4
lincis <linardssils1@gmail.com>
2023-12-10 00:01:07 +0200

    Bit more responsive

--- client/src/_root/pages/Home.jsx
+++ client/src/_root/pages/Home.jsx
@@ -23,20 +23,20 @@ const Home = () => {
 
   return (
     <ParallaxProvider>
-      <main className="flex flex-col relative min-h-screen sm:justify-center p-4 md:h-full md:px-8 lg:px-16 max-w-7xl m-auto overflow-hidden">
+      <main className="flex flex-col relative min-h-screen justify-center p-4 md:h-full md:px-8 lg:px-16 max-w-7xl m-auto overflow-hidden">
         <div className={`z-10 ${isLoaded ? 'animate-fadeIn' : ''}`} ref={ref1}>
           <div className="mx-auto max-w-7xl mb-8 sm:mb-48">
-            <h1 className="text-center text-8xl sm:text-9xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-white via-black to-blue-300 animate-text block">
+            <h1 className="text-center text-8xl sm:text-9xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-bmw-blue via-bmw-dark-blue to-bmw-red animate-text block">
               BMW
             </h1>
-            <h1 className="text-center text-7xl sm:text-8xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-white via-black to-blue-300 animate-text block mb-4">
+            <h1 className="text-center text-7xl sm:text-8xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-bmw-blue via-bmw-dark-blue to-bmw-red animate-text block mb-4">
               FANPAGE
             </h1>
           </div>
         </div>
       </main>
       <div className="overflow-hidden flex flex-col relative min-h-screen sm:justify-center p-4 md:h-full md:px-8 lg:px-16 max-w-7xl m-auto">
-        <div className="mx-auto max-w-7xl mb-8 sm:mb-48 h-1/2">
+        <div className="mx-auto max-w-7xl mb-8 sm:mb-48 ">
           <Description containerRef={ref1} inView={inView1} />
         </div>
         <div className="mx-auto max-w-7xl mb-8 sm:mb-48">
