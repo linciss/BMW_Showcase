af7a73b1fc1788bee1b4ff33837c9b572bdfe695
lincis <linardssils1@gmail.com>
2023-12-10 17:45:46 +0200

    Bit of fixing

--- backend/data/description.js
+++ backend/data/description.js
@@ -1,15 +1,15 @@
 const desc = [
   {
     id: 1,
-    bg: '/assets/bg1.png',
-    fg: '/assets/m3.png',
+    bg: '/public/assets/bg1.png',
+    fg: '/public/assets/m3.png',
     series: 'M3',
     text: 'Are you interested to see what each of BMW models are capable of?',
   },
   {
     id: 2,
-    bg: '/assets/bg1.png',
-    fg: '/assets/m4.png',
+    bg: '/public/assets/bg1.png',
+    fg: '/public/assets/m4.png',
     series: 'M4',
     text: 'Are you interested to see what each of BMW models are capable of?',
   },
