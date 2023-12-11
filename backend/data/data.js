6953b7d8b56e9249fd1f721b09353a5f719cb33e
lincis <linardssils1@gmail.com>
2023-12-11 20:26:34 +0200

    Addinga  description section because needed for card component

--- backend/data/data.js
+++ backend/data/data.js
@@ -14,6 +14,8 @@ const data = [
     ],
     bodyStyle: ['Coupe', 'Convertible', 'Sedan', 'Touring'],
     slug: 'e36',
+    description:
+      "The BMW E36, produced from 1990 to 2000, epitomizes BMW's 3 Series with timeless design, dynamic handling, and versatile body styles.",
   },
   {
     id: 2,
@@ -30,6 +32,8 @@ const data = [
     ],
     bodyStyle: ['Coupe', 'Convertible', 'Sedan', 'Touring'],
     slug: 'e46',
+    description:
+      'The BMW E46, spanning 1998 to 2006, epitomizes driving pleasure with iconic design, dynamic performance, and diverse body styles.',
   },
   {
     id: 3,
@@ -45,7 +49,10 @@ const data = [
       '5-speed automatic',
     ],
     bodyStyle: ['Coupe', 'Convertible', 'Sedan', 'Touring'],
+
     slug: 'e90',
+    description:
+      'The BMW E90 (2005–2013) redefines luxury with sleek design, advanced technology, and a harmonious blend of performance and comfort.',
   },
   {
     id: 4,
@@ -62,6 +69,8 @@ const data = [
     ],
     bodyStyle: ['Coupe', 'Convertible', 'Sedan', 'Touring'],
     slug: 'e92',
+    description:
+      'The BMW E92 (2006–2013) embodies sportiness, featuring a distinctive coupe design, powerful engines, and a thrilling driving experience.',
   },
   {
     id: 5,
@@ -72,6 +81,9 @@ const data = [
     engine: ['1.6', '1.8', '1.9', '2.0', '2.5', '2.8', '3.0', '3.2'],
     transmission: ['5-speed manual', '6-speed manual', '4-speed automatic'],
     bodyStyle: ['Coupe', 'Convertible', 'Sedan', 'Touring'],
+    slug: 'e93',
+    description:
+      'The BMW E93 (2007–2013) delivers open-top exhilaration, combining convertible elegance with dynamic performance and advanced engineering.',
   },
 ];
 export default data;
