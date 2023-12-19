a01ea5272bb2f68ec227544e9bf713dcdedc2413
lincis <linardssils1@gmail.com>
2023-12-19 16:03:14 +0200

    About page

--- client/src/_root/pages/About.jsx
+++ client/src/_root/pages/About.jsx
@@ -1,7 +1,46 @@
+// About.js
 import React from 'react';
 
 const About = () => {
-  window.scrollTo(0, 0);
-  return <div>About</div>;
+  return (
+    <div className="container mx-auto my-8 p-4 bg-white rounded-lg max-w-7xl">
+      <h1 className="text-4xl font-bold mb-4 text-center">
+        About My BMW Obsession
+      </h1>
+      <p className="text-lg mb-4">
+        Hello BMW enthusiasts and fellow car lovers! And welcome to my little
+        corner of the internet dedicated to the fascinating world of BMW models.
+      </p>
+      <p className="text-lg mb-4">
+        I started this website because of my deep passion for BMWs. Whether
+        you're a fellow enthusiast or just curious about BMWs, you're in the
+        right place. Here, I share insights, and information about various BMW
+        series and models.
+      </p>
+      <h2 className="text-2xl font-bold mb-2 text-center">Why BMWs?</h2>
+      <p className="text-lg mb-4">
+        I fell in love with BMWs for their combination of sleek design,
+        precision engineering, and the sheer joy of driving. This site is my way
+        of sharing that love and knowledge with you.
+      </p>
+      <h2 className="text-2xl font-bold mb-2 text-center">
+        What You'll Find Here
+      </h2>
+      <p className="text-lg mb-4">
+        Explore detailed information, specs, and even a bit of history about
+        different BMW models. This isn't a sales pitch, it's a celebration of
+        automotive craftsmanship.
+      </p>
+      <h2 className="text-2xl font-bold mb-2 text-center">Connect With Me</h2>
+      <p className="text-lg mb-4">
+        This website is a labor of love, and I'd love to connect with fellow BMW
+        enthusiasts. Feel free to drop me a message or connect with me on social
+        media. Let's share our passion for BMWs!
+      </p>
+      <h2 className="text-2xl font-bold mb-2 text-center ">Contact</h2>
+      N/A Right now
+    </div>
+  );
 };
+
 export default About;
