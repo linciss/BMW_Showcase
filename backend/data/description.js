a01b8369180d015d05bb91a50467cfaf8aabd5b6
lincis <linardssils1@gmail.com>
2023-12-19 13:17:56 +0200

    change of descriptions

--- backend/data/description.js
+++ backend/data/description.js
@@ -4,14 +4,14 @@ const desc = [
     bg: '/public/assets/bg1.png',
     fg: '/public/assets/m3.png',
     series: 'M3',
-    text: 'Are you interested to see what each of the BMW models are capable of?',
+    text: 'Are you interested to see what each of BMW models are capable of?',
   },
   {
     id: 2,
     bg: '/public/assets/bg1.png',
     fg: '/public/assets/m4.png',
     series: 'M4',
-    text: 'Are you interested to see what each of BMW models are capable of?',
+    text: 'Are you fan of BMW cars? Check out the variety of models that we offer!',
   },
 ];
 export default desc;
